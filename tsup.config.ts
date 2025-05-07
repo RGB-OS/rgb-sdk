bcedb73ea34eca9418fa20a29af7fce902bb8dae
Yurii Bandrivskyi <yuriibandrivskyi@MacBook-Pro.local>
2025-05-07 18:04:28 +0300

    update configs

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'tsup';
-
+import { copyFileSync } from 'fs';
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['cjs'],
   outDir: 'dist',
   target: 'node18',
   dts: true,
-  outExtension: () => ({ js: '.cjs' })
+  outExtension: () => ({ js: '.cjs' }),
+  onSuccess: async () => {
+    copyFileSync('src/bitcoindevkit_bg.wasm', 'dist/bitcoindevkit_bg.wasm');
+  },
 });
