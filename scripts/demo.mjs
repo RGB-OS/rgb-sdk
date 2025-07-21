cdeb2fbd7580b6a2125551ddb0cfec7d34e691c0
Yurii Bandrivskiy <bandrivskiy96@gmail.com>
2025-07-21 10:52:00 +0300

    update rgb lib

--- scripts/demo.mjs
+++ scripts/demo.mjs
@@ -3,13 +3,7 @@
 import { wallet ,createWallet} from "../dist/index.cjs";
 // const keys = createWallet();
 // console.log('keys', JSON.stringify(keys));
-const testData = {
-    "mnemonic": "input asset pistol aware deliver imitate sausage behave news category manual catch",
-    "xpub": "tpubD6NzVbkrYhZ4XSMh32ufPtwNcCEXR2UPtWPKH6cr2sW4GckunPiZt3Kt1Nn8ssYPSyfaEXMHKBTgRomEe2ZiCbeevAGBBkQMmRDca8DYxhb",
-    "account_xpub_vanilla": "tpubDC3SyMdeKCosZC2J2mk72m4Xp5mikH9VzTh6sVejVww3LS19HPrtLmcoyEQrbzxRcQtPQntDg5vkXuuah7QLhLKsvEvUU4B7QioM7Xc5D8K",
-    "account_xpub_colored": "tpubDCW9yjx496cAwMCKSEtgnsrYgJBzZgvzzcvKsvitJTugdzF4FM3YUpKwZZ6P2L9XDhNtbHLyH7PvRN7GumcubAqyQQ3dctjaATVJaqajxa3",
-    "master_fingerprint": "cf01173d"
-}
+
 wallet.init({
     mnemonic:testData.mnemonic,
     xpub_van:testData.account_xpub_vanilla,
@@ -17,7 +11,7 @@ wallet.init({
     master_fingerprint:testData.master_fingerprint,
     network:"Regtest"
 });
-const signed = await wallet.signPsbt("cHNidP8BAP0KAQIAAAABsVqOKpV60NTT/5ClnR6wlouiFZp4/9ntF9haf3Df2/4CAAAAAP3///8FsSgHAAAAAAAiUSAbJMxO/lj9xgL00+bM6JU3ejGRNlOyxQgWAzEDE6zPYOgDAAAAAAAAIlEgmM03ecPNc3WH5vnjx3JF6Wv9WffcM4zlLBtVD6xv3z3oAwAAAAAAACJRIELTHjNseFJVeOSNnJALgsitE/wr187sRlo+aCslwvse6AMAAAAAAAAiUSBy647w5+m9hoW2ZSLe/WRMyH+bR1muJuvlCrt24MN7BugDAAAAAAAAIlEgGKLZ6WpEIZq1NTaBrrLotMkh/WtDErcIP/jAADOmy1hvrgUAAAEBK9o9BwAAAAAAIlEgcgZwPhCfsVHmxRm+UQUKzoYmouvuzijNsmNxTqs9J/0hFpn6m9nMYlp+H1A16sY4K66AJLQqfABR0jYc80wGMY5GGQDPARc9VgAAgAEAAIAAAACAAQAAAAAAAAABFyCZ+pvZzGJafh9QNerGOCuugCS0KnwAUdI2HPNMBjGORgABBSCQGLR2a1EH2gSTrpEEc7Zi824OGZf0GqpkdzwiCU1IniEHkBi0dmtRB9oEk66RBHO2YvNuDhmX9BqqZHc8IglNSJ4ZAM8BFz1WAACAAQAAgAAAAIABAAAAAQAAAAABBSBIl1elNuZZULkq9EoDS+4r5m0SRkQla6+Yl6bzRdW7lyEHSJdXpTbmWVC5KvRKA0vuK+ZtEkZEJWuvmJem80XVu5cZAM8BFz1WAACAH58MgAAAAIAAAAAAXAAAAAABBSBTQ1uoiVZAezlI2/5IUpvwjf/SzsJA0ijtRDRhq7Zr3CEHU0NbqIlWQHs5SNv+SFKb8I3/0s7CQNIo7UQ0Yau2a9wZAM8BFz1WAACAH58MgAAAAIAAAAAAXQAAAAABBSAjo2p8ktX7XLGURgCyAsEOwa8Zqrob6f29QHWtZeG3JyEHI6NqfJLV+1yxlEYAsgLBDsGvGaq6G+n9vUB1rWXhtycZAM8BFz1WAACAH58MgAAAAIAAAAAAWgAAAAABBSDwO/lT/KF+JEaiC78J9Bl5d+ylMO6nbvpBLcZ9ie+2LCEH8Dv5U/yhfiRGogu/CfQZeXfspTDup276QS3GfYnvtiwZAM8BFz1WAACAH58MgAAAAIAAAAAAWwAAAAA=");
+const signed = await wallet.signPsbt(psbtToSign);
 console.log('signed', signed);
 // const mnemonic = 'world flee sword train ready audit wedding opinion boil drift detect total'
 // const mnemonic='melody fee hero onion rapid bullet again exile exact exile slogan grace'
